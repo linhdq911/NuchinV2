92531c2b6d1a168bba9568b0d6c60af9b940d510
@quanglinh1 <quanglinh@media-one.vn>
2022-07-15 17:12:08 +0700

    'calen_footer'

--- screens/Calen.js
+++ screens/Calen.js
@@ -1,4 +1,12 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  ScrollView
+} from 'react-native';
 import React from 'react';
 import {Calendar} from 'react-native-calendars';
 import DatePicker from 'react-native-styled-datepicker';
@@ -8,7 +16,7 @@ const Calen = () => {
     <View style={{alignItems: 'center'}}>
       <View
         style={{
-          height: '73%',
+          height: '55%',
           width: '90%',
           backgroundColor: 'red',
           borderRadius: 25,
@@ -16,9 +24,53 @@ const Calen = () => {
         }}>
         <DatePicker weekendDateColor={'#fff'} disabledDateColor={'gray'} />
       </View>
-      <View>
-        <Text>Món hôm nay</Text>
+
+      <View style={{top: '7%'}}>
+        <Text style={{fontSize: 18, color: '#3D2B2B'}}>Món hôm nay</Text>
       </View>
+
+      <View
+        style={{
+          top: '15%',
+          flexDirection: 'row',
+          backgroundColor: '#FFFFFF',
+          borderRadius: 25,
+        }}>
+        <View style={{}}>
+          <Image
+            source={require('../image/Rectangle1800.png')}
+            style={{marginVertical: '10%', marginHorizontal: 10}}
+          />
+        </View>
+        <View>
+          <Text>Bánh mì Sài Gòn</Text>
+          <Text>Lorem ipsum dolor sit amet, consectetur </Text>
+        </View>
+      </View>
+
+<View style={{top: '7%'}}>
+        <Text style={{fontSize: 18, color: '#3D2B2B'}}>Món hôm nay</Text>
+      </View>
+
+      <View
+        style={{
+          top: '15%',
+          flexDirection: 'row',
+          backgroundColor: '#FFFFFF',
+          borderRadius: 25,
+        }}>
+        <View style={{}}>
+          <Image
+            source={require('../image/Rectangle1800.png')}
+            style={{marginVertical: '10%', marginHorizontal: 10}}
+          />
+        </View>
+        <View>
+          <Text>Bánh mì Sài Gòn</Text>
+          <Text>Lorem ipsum dolor sit amet, consectetur </Text>
+        </View>
+      </View>
+      
     </View>
   );
 };
